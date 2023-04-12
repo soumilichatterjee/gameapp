989a411da516d59c61c21873ed2a8783f5badcee
Soumili Chatterjee <riyachatterjee403@gmail.com>
2023-04-12 20:29:52 +0530

    Native

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,36 +1,49 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-import { View, TextInput,StyleSheet,Alert } from "react-native";
-import PrimaryButton from "../components/PrimaryButton";
+import { View, TextInput, StyleSheet, Alert } from "react-native";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Colors from "../constants/colors";
 
-function StartGameScreen() {
-  const [enteredNumber,setEnteredNumber] = useState('');
+function StartGameScreen({ onPickNumber }) {
+  const [enteredNumber, setEnteredNumber] = useState("");
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
   }
 
-  function resetInputHandler(){
-    setEnteredNumber('')
+  function resetInputHandler() {
+    setEnteredNumber("");
   }
 
   function confirmInputHandler() {
-const  chosenNumber= parseInt(enteredNumber);
-if(isNaN(chosenNumber) || chosenNumber<=0 || chosenNumber>99 ){
-Alert.alert('Invalid number!', 'Number has to be a number between 1 and 99', [{text:'Okay',style:'destructive', onPress:resetInputHandler}]);
-return;
-}
-console.log('Valid Number');
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+      );
+      return;
+    }
+    onPickNumber(chosenNumber);
   }
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" value={enteredNumber}  onChangeText ={numberInputHandler} autoCorrect={false}/>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        value={enteredNumber}
+        onChangeText={numberInputHandler}
+        autoCorrect={false}
+      />
       <View style={styles.buttonsContainer}>
-      <View style={styles.buttonContainer}>
-      <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-      </View>
-      <View style={styles.buttonContainer}>
-      <PrimaryButton onPress={confirmInputHandler} >Confirm</PrimaryButton>
-      </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
       </View>
     </View>
   );
@@ -39,37 +52,36 @@ console.log('Valid Number');
 const styles = StyleSheet.create({
   inputContainer: {
     // flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-    marginTop:100,
-    marginHorizontal:24,
-    padding:16,
-    backgroundColor:'#3b021f',
-    borderRadius:8,
-    elevation:4,
-    shadowColor:'black',
-    shadowOffset:{width:0,height:2},
-    shadowRadius:6,
-    shadowOpacity:0.25
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
   },
-  numberInput:{
-height:50,
-width:50,
-fontSize:32,
-borderBottomColor:'#ddb52f',
-borderBottomWidth:2,
-color:'#ddb52f',
-marginVertical:8,
-fontWeight:'bold',
-textAlign:'center',
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: Colors.accent500,
+    borderBottomWidth: 2,
+    color: Colors.accent500,
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
-  buttonsContainer:{
-    flexDirection:'row',
+  buttonsContainer: {
+    flexDirection: "row",
   },
-  buttonContainer:{
-    flex:1
-  }
-})
-
+  buttonContainer: {
+    flex: 1,
+  },
+});
 
 export default StartGameScreen;
